758f8afad6f68e8d759be2f8e034d775ec63c293
Stephen Leong Koan <sleongkoan@gmail.com>
2014-03-25 09:42:03 -0400

    do some cleanup

--- gzip_all.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print "cd /u/leongs/reproduction_projet_naim/rel20/3D/failed_processed"
-
-for elem in os.listdir("/u/leongs/reproduction_projet_naim/rel20/3D/failed_processed"):
-    if elem.endswith(".pdb"):
-        print "gzip " + os.path.join("/u/leongs/reproduction_projet_naim/rel20/3D/failed_processed", elem)
